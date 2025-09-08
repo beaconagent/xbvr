020c0778cb5948ad2b43568c4cfee42a6b9536dc
beaconagent <linkin_pain@yahoo.com>
2025-09-09 02:15:06 +0300

    limit concurrent requests to 10

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -28,6 +28,7 @@ import (
 	"github.com/xbapps/xbvr/pkg/config"
 	"github.com/xbapps/xbvr/pkg/migrations"
 	"github.com/xbapps/xbvr/pkg/models"
+	"github.com/xbapps/xbvr/pkg/server"
 	"github.com/xbapps/xbvr/pkg/session"
 	"github.com/xbapps/xbvr/pkg/tasks"
 	"github.com/xbapps/xbvr/ui"
@@ -162,6 +163,10 @@ func StartServer(version, commit, branch, date string) {
 
 	// CORS
 	handler := cors.Default().Handler(r)
+
+	// Global request concurrency limit
+	concurrentLimit := 10
+	handler = QueueMiddleware(concurrentLimit, handler)
 
 	// WAMP router
 	routerConfig := &router.Config{
