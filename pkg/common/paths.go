e7e345aeaa4489a772a5b59b9962079b3da6e883
beaconagent <linkin_pain@yahoo.com>
2025-10-12 23:36:46 +0300

    add requests timeout as configurable variable

--- pkg/common/paths.go
+++ pkg/common/paths.go
@@ -57,6 +57,7 @@ func InitPaths() {
 	ws_addr := flag.String("ws_addr", "", "Optional: override default Websocket address from the default 0.0.0.0:9998")
 	db_connection_pool_size := flag.Int("db_connection_pool_size", 0, "Optional: sets a limit to the number of db connections while scraping")
 	concurrentSscrapers := flag.Int("concurrent_scrapers", 0, "Optional: sets a limit to the number of concurrent scrapers")
+	requestsTimeout := flag.Int("requests_timeout", 0, "Optional: sets the timeout in ms for internal requests")
 
 	// InitPaths runs from models' package init, which means it also runs inside test
 	// binaries. `go test` passes -test.* flags that this flag set does not define, and
@@ -135,6 +136,11 @@ func InitPaths() {
 		ConcurrentScrapers = *concurrentSscrapers
 	} else {
 		ConcurrentScrapers = EnvConfig.ConcurrentScrapers
+	}
+	if *requestsTimeout != 0 {
+		RequestsTimeout = *requestsTimeout
+	} else {
+		RequestsTimeout = EnvConfig.RequestsTimeout
 	}
 
 	_ = os.MkdirAll(AppDir, os.ModePerm)
